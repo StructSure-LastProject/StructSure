77f2ea68d90d35357b7813dbc0643fe13517d623
Vinodcoumar JAAYARADJE <78848834+Vinodjr@users.noreply.github.com>
2024-12-06 20:55:40 +0100

    project structure modified

--- Backend/src/main/java/fr/uge/structsure/StructSureBackendApplication.java
+++ Backend/src/main/java/fr/uge/structsure/StructSureBackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StructsureBackendApplication {
+public class StructSureBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StructsureBackendApplication.class, args);
+		SpringApplication.run(StructSureBackendApplication.class, args);
 	}
 
 }
