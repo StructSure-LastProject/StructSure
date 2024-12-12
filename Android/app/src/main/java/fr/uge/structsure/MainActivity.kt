439331971145cc3942ec49617d577688cb8a8dcb
infoDev1 <achrafqabil2002@gmail.com>
2024-12-12 02:16:22 +0100

    Add Summary view

--- Android/app/src/main/java/fr/uge/structsure/MainActivity.kt
+++ Android/app/src/main/java/fr/uge/structsure/MainActivity.kt
@@ -2,13 +2,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import fr.uge.structsure.ui.components.HeaderView
+import fr.uge.structsure.ui.components.StructureSummaryView
 import fr.uge.structsure.ui.theme.StructSureTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +22,10 @@ class MainActivity : ComponentActivity() {
         setContent {
             StructSureTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    HeaderView(modifier = Modifier.padding(innerPadding))
+                    Column(modifier = Modifier.padding(innerPadding)) {
+                        HeaderView()
+                        StructureSummaryView()
+                    }
                 }
             }
         }
@@ -31,5 +37,16 @@ class MainActivity : ComponentActivity() {
 fun HeaderViewPreview() {
     StructSureTheme {
         HeaderView()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun StructureSummaryViewPreview() {
+    StructSureTheme {
+        Column(modifier = Modifier.padding(16.dp)) {
+            HeaderView()
+            StructureSummaryView()
+        }
     }
 }
