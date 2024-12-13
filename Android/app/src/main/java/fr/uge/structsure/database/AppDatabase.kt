9141ef69a4f178253c5e63e0e227c897fae07278
infoDev1 <achrafqabil2002@gmail.com>
2024-12-13 14:56:35 +0100

    Add Room database for scans and sensors with basic functionality

--- Android/app/src/main/java/fr/uge/structsure/database/AppDatabase.kt
+++ Android/app/src/main/java/fr/uge/structsure/database/AppDatabase.kt
@@ -6,21 +6,31 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import fr.uge.structsure.dbTest.data.UserData
 import fr.uge.structsure.dbTest.data.UserDao
+import fr.uge.structsure.start_scan.data.ScanEntity
+import fr.uge.structsure.start_scan.data.SensorEntity
+import fr.uge.structsure.start_scan.data.dao.ScanDao
 
-@Database(entities = [UserData::class], version = 1)
-abstract class AppDatabase: RoomDatabase(){
+// Add entities to the database
+@Database(
+    entities = [UserData::class, ScanEntity::class, SensorEntity::class],
+    version = 2,
+    exportSchema = false // Désactive l'exportation des schémas c'est qu'un prototype
+)
+abstract class AppDatabase : RoomDatabase() {
 
-    companion object{
+    companion object {
         @Volatile
         private var INSTANCE: AppDatabase? = null
 
         fun getDatabase(context: Context): AppDatabase {
-            return INSTANCE ?: synchronized(this){
+            return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     AppDatabase::class.java,
                     "STRUCTSURE_DB"
-                ).allowMainThreadQueries().build()
+                )
+                    .fallbackToDestructiveMigration() // Option qu'on peut utiliser pour éviter des erreurs de migration pendant le développement.
+                    .build()
                 INSTANCE = instance
                 instance
             }
@@ -28,4 +38,5 @@ abstract class AppDatabase: RoomDatabase(){
     }
 
     abstract fun userDao(): UserDao
+    abstract fun scanDao(): ScanDao
 }
