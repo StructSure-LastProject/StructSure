881d33794df682fffe33fc71a94c7e22a28cd2cc
Marc Le Coquil <ristoufletouille@gmail.com>
2024-12-13 16:44:04 +0100

    Fix ScanSummaryView. Default sensor count still present.

--- Android/app/src/main/java/fr/uge/structsure/start_scan/presentation/components/sensors/status/ScanSummaryView.kt
+++ Android/app/src/main/java/fr/uge/structsure/start_scan/presentation/components/sensors/status/ScanSummaryView.kt
@@ -15,11 +15,14 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.zIndex
 import fr.uge.structsure.R
 import fr.uge.structsure.start_scan.domain.ScanViewModel
+import fr.uge.structsure.start_scan.presentation.components.sensors.status.SensorState
+import fr.uge.structsure.start_scan.presentation.components.sensors.status.SensorStatus
 
 
 @Composable
@@ -70,6 +73,8 @@ fun StructureSummaryView(viewModel: ScanViewModel) {
     }
 }
 
+
+@Preview(showBackground = true)
 @Composable
 fun SensorStatusColumn() {
     Row(
@@ -77,46 +82,22 @@ fun SensorStatusColumn() {
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.Start
     ) {
-        repeat(4) { index ->
-            SensorStatusCircle(sensorNumber = index + 21, sensorState = SensorState.values()[index % 4])
-            Spacer(modifier = Modifier.height(8.dp))
-        }
+        // TODO
+        SensorStatus(
+            SensorState.OK,
+            "27"
+        )
+        SensorStatus(
+            SensorState.NOK,
+            "12"
+        )
+        SensorStatus(
+            SensorState.DEFECTIVE,
+            "0"
+        )
+        SensorStatus(
+            SensorState.UNSCAN,
+            "171"
+        )
     }
-}
-
-
-@Composable
-fun SensorStatusCircle(sensorNumber: Int, sensorState: SensorState) {
-    Box(
-        modifier = Modifier
-            .size(width = 80.dp, height = 40.dp)
-            .background(Color.White, shape = RoundedCornerShape(20.dp)),
-        contentAlignment = Alignment.Center
-    ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center,
-            modifier = Modifier.padding(8.dp)
-        ) {
-            Box(
-                modifier = Modifier
-                    .size(20.dp)
-                    .background(sensorState.color, shape = RoundedCornerShape(10.dp))
-            )
-            Spacer(modifier = Modifier.width(8.dp))
-            Text(
-                text = sensorNumber.toString(),
-                color = Color.Black,
-                fontSize = 16.sp,
-                fontWeight = FontWeight.Bold
-            )
-        }
-    }
-}
-
-enum class SensorState(val color: Color) {
-    GREEN(Color.Green),
-    RED(Color.Red),
-    ORANGE(Color(0xFFFFA500)), // Orange color
-    GRAY(Color.Gray)
 }
