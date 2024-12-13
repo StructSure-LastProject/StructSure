109f76f39d8b8c9d851d8b40206bd3cd4b4f4788
Marc Le Coquil <ristoufletouille@gmail.com>
2024-12-13 01:18:11 +0100

    Graphical corrections of the list provided.

--- Android/app/src/main/java/fr/uge/structsure/start_scan/presentation/components/sensorsview/SensorItem.kt
+++ Android/app/src/main/java/fr/uge/structsure/start_scan/presentation/components/sensorsview/SensorItem.kt
@@ -15,10 +15,13 @@ import fr.uge.structsure.ui.theme.Ok
 import fr.uge.structsure.ui.theme.Red
 import fr.uge.structsure.ui.theme.Unknown
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
 import fr.uge.structsure.ui.theme.Typography
 
+
+@Preview(showBackground = true)
 @Composable
-fun SensorItem(sensorName: String, state: Int) {
+fun SensorItem(sensorName: String="Default", state: Int=-1) {
     // Définit les couleurs en fonction de l'état
     val stateColor = when (state) {
         0 -> Ok         //OK
@@ -28,15 +31,15 @@ fun SensorItem(sensorName: String, state: Int) {
         else -> Color.Gray
     }
 
-    Card(
+    Box(
         modifier = Modifier
-            .padding(8.dp)
-            .fillMaxWidth(),
+            .padding(4.dp)
+            .fillMaxWidth()
     ) {
         Row(
             verticalAlignment = Alignment.CenterVertically,
             horizontalArrangement = Arrangement.Start,
-            modifier = Modifier.padding(16.dp)
+            modifier = Modifier.padding(8.dp)
         ) {
             Box(
                 modifier = Modifier
