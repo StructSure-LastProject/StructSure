5f33bd7c3c46b93e5a488b8e1f91a3611e1652af
Anass Ousaid <anassousaid@Anasss-MacBook-Air.local>
2024-12-17 12:39:19 +0100

    Login Front is being programmed 30% done

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,14 +1,38 @@
 /* @refresh reload */
-import { render } from 'solid-js/web'
+import { render } from 'solid-js/web';
+import './Login.css';
+import logo from '../assets/logo.svg';
 
 function Login() {
-
-
     return (
         <>
-            
+        <div class="bg-gray-50 h-screen">
+            <div class="p-25px mb-35px">
+                <div class="flex justify-center">
+                    <img src={logo} class="w-236px" alt="Vite logo" />
+                </div>
+            </div>
+            <div class="px-25px">
+                <div class="bg-white w-500px h-394px flex flex-col gap-x-25px pr-5 pl-5">
+                    <div class="">
+                        <h1>Connexion</h1>
+                        <p class="w-276px size-3.5 text-sm">Renseignez vos identifiants StructSure pour accéder à l'application.</p>
+                    </div>
+                    <div>
+                        
+                    </div>
+
+                    <div>
+                        
+                    </div>
+                    <div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
         </>
     );
 }
 
-render(() => {Login}, document.getElementById('root'))
+export default Login
