26237616b50e9ee36a210fa945e98da324d0ca4e
Anass Ousaid <anassousaid@Anasss-MacBook-Air.local>
2024-12-17 12:39:19 +0100

    Login Front is being programmed 30% done

--- Frontend/src/index.jsx
+++ Frontend/src/index.jsx
@@ -1,8 +1,17 @@
 /* @refresh reload */
-import { render } from 'solid-js/web'
-import './index.css'
-import App from './App.jsx'
+import { render } from 'solid-js/web';
+import { Router } from "@solidjs/router";
+import './index.css';
+import Login from './pages/Login.jsx';
 
 const root = document.getElementById('root')
 
-render(() => <App />, root)
+
+const App = (props) => (
+    <>
+      <h1>Site Title</h1>
+      {props.children}
+    </>
+  );
+
+render(() => <Login />, root)
