62d550c34b0b527d76776efb58a85eccb70566a2
Anass Ousaid <anassousaid@Anasss-MacBook-Air.local>
2024-12-17 10:31:02 +0100

    Integration de tailwind css

--- Backend/src/main/java/fr/uge/structsure/config/CustomUserDetailsService.java
+++ Backend/src/main/java/fr/uge/structsure/config/CustomUserDetailsService.java
@@ -1,6 +1,5 @@
 package fr.uge.structsure.config;
 
-import fr.uge.structsure.entities.Account;
 import fr.uge.structsure.repositories.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -8,7 +7,6 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import java.util.Collections;
