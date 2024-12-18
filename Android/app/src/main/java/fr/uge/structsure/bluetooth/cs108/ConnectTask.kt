9641785cfb7d36e404460d1c2e8b198072fcb9f5
AurelFi <aurelien.fiche.esipe@gmail.com>
2024-12-18 22:43:00 +0100

    Adding a loop to detect when the device is ready to scan chips

--- Android/app/src/main/java/fr/uge/structsure/bluetooth/cs108/ConnectTask.kt
+++ Android/app/src/main/java/fr/uge/structsure/bluetooth/cs108/ConnectTask.kt
@@ -3,10 +3,17 @@ package fr.uge.structsure.bluetooth.cs108
 import android.os.AsyncTask
 import com.csl.cslibrary4a.ReaderDevice
 import fr.uge.structsure.MainActivity
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.util.concurrent.CancellationException
 
 class ConnectTask(private var connectingDevice: ReaderDevice, private val callback: (d: ReaderDevice?) -> Unit) : AsyncTask<Void, String, Int>() {
     private var waitTime: Int = 30
     private var setting = -1
+    private var task: Job? = null
 
     override fun onPreExecute() {
         super.onPreExecute()
@@ -60,6 +67,22 @@ class ConnectTask(private var connectingDevice: ReaderDevice, private val callba
         //        readerListAdapter.notifyDataSetChanged();
         println("TOAST - BLE is connected")
         callback(connectingDevice)
+        if (task == null) watchReadyState()
         super.onPostExecute(result)
+    }
+
+    @OptIn(DelicateCoroutinesApi::class)
+    private fun watchReadyState() {
+        task = GlobalScope.launch {
+            try {
+                while (task!!.isActive && MainActivity.csLibrary4A.mrfidToWriteSize() != 0) {
+                    delay(500L)
+                }
+            } catch (e: CancellationException) {
+                // Interrupted
+            }
+            println("[ConnectTask] Device is ready!")
+            task = null
+        }
     }
 }
