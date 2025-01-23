7d64fb66d51d8175da3ade28d03fe8eb379a51ad
AurelFi <aurelien.fiche.esipe@gmail.com>
2025-01-23 12:35:51 +0100

    Header static

--- Android/app/src/main/java/fr/uge/structsure/startScan/presentation/sensors/status/ScanSummaryView.kt
+++ Android/app/src/main/java/fr/uge/structsure/startScan/presentation/sensors/status/ScanSummaryView.kt
@@ -1,114 +1,113 @@
 package fr.uge.structsure.startScan.presentation.components
 
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.border
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import fr.uge.structsure.R
+import fr.uge.structsure.components.Button
 import fr.uge.structsure.startScan.domain.ScanViewModel
 import fr.uge.structsure.startScan.presentation.sensors.status.SensorState
-import fr.uge.structsure.ui.theme.*
+import fr.uge.structsure.ui.theme.LightGray
+import fr.uge.structsure.ui.theme.White
 
+@Preview
 @Composable
-fun StructureSummaryView(viewModel: ScanViewModel) {
+fun StructureSummaryPreview() {
+    Box(Modifier.background(LightGray)) {
+        StructureSummaryView(null)
+    }
+}
+
+/**
+ * Header of the Scan page that contains the name of the structure,
+ * the button to edit its note and the structure weather.
+ */
+@Composable
+fun StructureSummaryView(viewModel: ScanViewModel?) {
     Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(16.dp)
+        modifier = Modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.spacedBy(10.dp, Alignment.Top),
+        horizontalAlignment = Alignment.Start,
+
     ) {
+        /* Structure name and note button */
         Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight()
-                .padding(start = 10.dp, top = 10.dp, end = 10.dp, bottom = 10.dp),
+            modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically
         ) {
             Text(
                 text = "Viaduc de Sylans",
-                style = TextStyle(
-                    fontSize = 20.sp,
-                    fontFamily = fonts,
-                    fontWeight = FontWeight(600),
-                    color = Black,
-                )
+                style = MaterialTheme.typography.titleLarge
             )
 
-            Row(
-                horizontalArrangement = Arrangement.spacedBy(0.dp, Alignment.CenterHorizontally),
-                verticalAlignment = Alignment.CenterVertically,
-                modifier = Modifier
-                    .background(color = White, shape = RoundedCornerShape(size = 50.dp))
-                    .padding(start = 10.dp, top = 10.dp, end = 10.dp, bottom = 10.dp)
-            ) {
-                Image(
-                    painter = painterResource(id = R.drawable.notepad_text),
-                    contentDescription = "image description",
-                    contentScale = ContentScale.None,
-                    modifier = Modifier
-                        .padding(1.dp)
-                        .width(25.dp)
-                        .height(25.dp)
-                )
+            Button(
+                R.drawable.notepad_text,
+                "Note de l'ouvrage",
+                background = White
+            )
+        }
+        /* Structure weather */
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.spacedBy(10.dp, Alignment.Start),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            repeat(4) { index ->
+                SensorWeather(index + 21, SensorState.entries[index % 4])
             }
         }
-        Spacer(modifier = Modifier.height(16.dp))
-        SensorStatusColumn()
     }
 }
 
+/**
+ * Small bean containing a colored circle and a number that tells how
+ * many sensor are contained in a given state.
+ * @param count the number of sensor for the given state
+ * @param state (weather) state to display
+ */
 @Composable
-fun SensorStatusColumn() {
+private fun SensorWeather(count: Int, state: SensorState) {
     Row(
-        modifier = Modifier.fillMaxWidth(),
-        verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.Start
-    ) {
-        repeat(5) { index ->
-            SensorStatusCircle(sensorNumber = index + 21, sensorState = SensorState.values()[index % 4])
-            Spacer(modifier = Modifier.height(8.dp))
-        }
-    }
-}
-
-
-@Composable
-fun SensorStatusCircle(sensorNumber: Int, sensorState: SensorState) {
-    Box(
         modifier = Modifier
-            .size(width = 80.dp, height = 40.dp)
-            .background(Color.White, shape = RoundedCornerShape(20.dp)),
-        contentAlignment = Alignment.Center
+             .horizontalScroll(rememberScrollState())
+            .height(40.dp)
+            .background(color = White, shape = RoundedCornerShape(size = 50.dp))
+            .padding(start = 15.dp, top = 5.dp, end = 15.dp, bottom = 5.dp),
+        horizontalArrangement = Arrangement.spacedBy(10.dp, Alignment.Start),
+        verticalAlignment = Alignment.CenterVertically,
     ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.Center,
-            modifier = Modifier.padding(8.dp)
-        ) {
-            Box(
-                modifier = Modifier
-                    .size(20.dp)
-                    .background(sensorState.color, shape = RoundedCornerShape(10.dp))
-            )
-            Spacer(modifier = Modifier.width(8.dp))
-            Text(
-                text = sensorNumber.toString(),
-                color = Color.Black,
-                fontSize = 16.sp,
-                fontWeight = FontWeight.Bold
-            )
-        }
+        Box(
+            modifier = Modifier
+                .size(16.dp)
+                .border(2.dp, state.color.copy(alpha = 0.25F), CircleShape)
+                .padding(2.dp)
+                .clip(CircleShape)
+                .background(state.color)
+        )
+        Text(
+            text = count.toString(),
+            style = MaterialTheme.typography.headlineMedium
+        )
     }
 }
