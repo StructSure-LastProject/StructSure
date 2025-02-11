83a87979ec719fc5bb7889cf2401b4d46f3012f6
ANASS OUSAID <anassousaidanass@gmail.com>
2025-02-11 16:13:54 +0100

    Api for getting the list of structures is now working and also the front end that shows these structures is now working

--- Backend/src/main/java/fr/uge/structsure/repositories/ResultRepository.java
+++ Backend/src/main/java/fr/uge/structsure/repositories/ResultRepository.java
@@ -1,8 +1,11 @@
 package fr.uge.structsure.repositories;
 
 import fr.uge.structsure.entities.Result;
+import fr.uge.structsure.entities.Structure;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ResultRepository extends JpaRepository<Result, Long> {}
+public interface ResultRepository extends JpaRepository<Result, Long> {
+}
