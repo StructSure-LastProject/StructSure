7b4d9e9b0dbef6361e46f5bdfd2defb0258bb943
ANASS OUSAID <anassousaidanass@gmail.com>
2025-02-11 16:13:54 +0100

    Api for getting the list of structures is now working and also the front end that shows these structures is now working

--- Backend/src/main/java/fr/uge/structsure/repositories/StructureRepository.java
+++ Backend/src/main/java/fr/uge/structsure/repositories/StructureRepository.java
@@ -16,25 +16,4 @@ public interface StructureRepository extends JpaRepository<Structure, Long> {
 
     Optional<Structure> findById(long id);
 
-    @Query("""
-        SELECT new fr.uge.structsure.dto.structure.AllStructureResponseDTO(
-        s.id,
-        s.name,
-        COALESCE(COUNT(DISTINCT sensor.sensorId), 0), 
-        COALESCE(COUNT(DISTINCT plan.id), 0),
-        CASE 
-             WHEN COUNT(DISTINCT sensor.sensorId) = 0 THEN 'UNKNOWN'
-             WHEN SUM(CASE WHEN result.state = fr.uge.structsure.entities.State.DEFAULTER THEN 1 ELSE 0 END) > 0 THEN 'DEFAULTER'
-             WHEN SUM(CASE WHEN result.state = fr.uge.structsure.entities.State.NOK THEN 1 ELSE 0 END) > 0 THEN 'NOK'
-             ELSE 'OK' 
-         END, 
-         s.archived
-        )
-        FROM Structure s
-        LEFT JOIN Sensor sensor ON sensor.structure = s 
-        LEFT JOIN Result result ON result.sensor = sensor 
-        LEFT JOIN Plan plan ON plan.structure = s 
-        GROUP BY s.id
-    """)
-    List<AllStructureResponseDTO> findStructuresWithState();
 }
