0ee09d571e088c48d6d31d657362ca45a9591cd0
Vinodjr <78848834+Vinodjr@users.noreply.github.com>
2025-02-12 15:34:25 +0100

    fix UI bug

--- Frontend/src/components/AdminPanel/EditAccountModal.jsx
+++ Frontend/src/components/AdminPanel/EditAccountModal.jsx
@@ -22,15 +22,15 @@ const EditAccountModal = ({ closeModal }) => {
     return (
         <div class="bg-gray-800 bg-opacity-50 backdrop-blur-[10px] shadow-[0px 0px 50px 0px #33333340] z-[100] bg-[#00000040] flex justify-center align-middle w-[100vw] h-[100vh] absolute top-0 left-0 p-[25px]">
             <div class="sm:text-start inset-0 relative flex flex-col w-[100%] max-w-[776px] size-fit rounded-[20px] p-[25px] gap-[15px] bg-white shadow-[0px 0px 50px 0px #33333340]">
-                <div class="flex justify-between items-center w-full sm:w-[726px] gap-[10px]">
-                    <h1 class="font-poppins text-[20px] sm:text-[25px] font-[600] leading-[30px] sm:leading-[37.5px] tracking-[0%]">
+                <div class="flex justify-between items-center w-full gap-[10px]">
+                    <h1 class="font-poppins text-[20px] sm:text-[20px] font-[600] leading-[30px] sm:leading-[37.5px] tracking-[0%]">
                         Edition de Compte
                     </h1>
-                    <div class="flex gap-[10px]">
-                        <button class="flex justify-center items-center w-[35px] h-[35px] sm:w-[40px] sm:h-[40px] rounded-[50px] bg-[#F133271A]">
+                    <div class="flex flex-wrap gap-[10px]">
+                        <button class="flex justify-center items-center w-[40px] h-[40px] sm:w-[40px] sm:h-[40px] rounded-[50px] bg-[#F133271A]">
                             <Trash2 color="#f13327" size={20} width={20} top={10} left={10}/>
                         </button>
-                        <button onClick={closeModal} class="flex justify-center items-center w-[35px] h-[35px] sm:w-[40px] sm:h-[40px] rounded-[50px] bg-[#F2F2F4]">
+                        <button onClick={closeModal} class="flex justify-center items-center w-[40px] h-[40px] sm:w-[40px] sm:h-[40px] rounded-[50px] bg-[#F2F2F4]">
                             <X />
                         </button>
                     </div>
@@ -40,22 +40,22 @@ const EditAccountModal = ({ closeModal }) => {
                     <p class="text-[#F13327] font-poppins HeadLineMedium">Error</p>
                 </div>
                 
-                <div class="sm:flex flex-wrap gap-[50px]">
-                <div class="flex flex-col w-full sm:w-[338px] gap-[15px]">
-                    <div class="flex flex-col gap-[5px]">
-                        <p class="font-poppins HeadLineMedium text-[#181818]">Nom*</p>
-                        <input value={lastName()} type="text" class="bg-[#F2F2F4] w-full h-[37px] rounded-[10px] py-[8px] px-[16px]" />
+                <div class="flex flex-wrap gap-[15px] lg:gap-[50px] text-[#181818] font-poppins">
+                    <div class="flex flex-col w-full lg:w-[338px] gap-[15px]">
+                        <div class="flex flex-col gap-[5px]">
+                            <p class="font-poppins HeadLineMedium text-[#181818]">Nom*</p>
+                            <input value={lastName()} type="text" class="bg-[#F2F2F4] w-full rounded-[10px] py-[8px] px-[16px]" />
+                        </div>
+                        <div class="flex flex-col gap-[5px]">
+                            <p class="font-poppins HeadLineMedium text-[#181818]">Prénom*</p>
+                            <input value={firstName()} type="text" class="bg-[#F2F2F4] w-full rounded-[10px] py-[8px] px-[16px]" />
+                        </div>
+                        <div class="flex flex-col gap-[5px]">
+                            <p class="font-poppins HeadLineMedium text-[#181818]">Identifiant*</p>
+                            <input value={id()} type="text" class="bg-[#F2F2F4] w-full h-[37px] rounded-[10px] py-[8px] px-[16px]" />
+                        </div>
                     </div>
-                    <div class="flex flex-col gap-[5px]">
-                        <p class="font-poppins HeadLineMedium text-[#181818]">Prénom*</p>
-                        <input value={firstName()} type="text" class="bg-[#F2F2F4] w-full h-[37px] rounded-[10px] py-[8px] px-[16px]" />
-                    </div>
-                    <div class="flex flex-col gap-[5px]">
-                        <p class="font-poppins HeadLineMedium text-[#181818]">Identifiant*</p>
-                        <input value={id()} type="text" class="bg-[#F2F2F4] w-full h-[37px] rounded-[10px] py-[8px] px-[16px]" />
-                    </div>
-                </div>
-                <div class="flex flex-col w-full sm:w-[338px] gap-[15px]">
+                <div class="flex flex-col w-full lg:w-[338px] gap-[15px]">
                     <div class="flex flex-col gap-[5px]">
                         <p class="font-poppins HeadLineMedium text-[#181818]">Mot de passe*</p>
                         <input value={password()} type="password" placeholder="*******" class="bg-[#F2F2F4] w-full h-[37px] rounded-[10px] py-[8px] px-[16px]" />
