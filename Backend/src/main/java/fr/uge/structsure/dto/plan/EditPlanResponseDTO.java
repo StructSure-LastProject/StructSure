2cb71f813b900550d72610bba6073f0988b09c9b
zakaria <zakaria.sellam@edu.esiee.fr>
2025-02-14 17:39:38 +0100

    first step -> add modularity to PlanService.java, start of plan edition backend

--- Backend/src/main/java/fr/uge/structsure/dto/plan/EditPlanResponseDTO.java
+++ Backend/src/main/java/fr/uge/structsure/dto/plan/EditPlanResponseDTO.java
@@ -2,12 +2,9 @@ package fr.uge.structsure.dto.plan;
 
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
-import java.util.Objects;
-
 @JsonSerialize
-public record AddPlanResponseDTO (long id, String createdAt) {
-    public AddPlanResponseDTO {
-        Objects.requireNonNull(createdAt);
+public record EditPlanResponseDTO(long id) {
+    public EditPlanResponseDTO {
         if (id <= 0) {
             throw new IllegalArgumentException("id must be greater than 0");
         }
