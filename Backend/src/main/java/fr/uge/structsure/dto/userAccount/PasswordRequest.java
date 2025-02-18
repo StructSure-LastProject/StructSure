7d644a46ac572de5142bc1d1515d1ec136710bb4
Vinodjr <78848834+Vinodjr@users.noreply.github.com>
2025-02-18 16:14:56 +0100

    remove useless record & refactor js code

--- Backend/src/main/java/fr/uge/structsure/dto/userAccount/PasswordRequest.java
+++
@@ -1,9 +0,0 @@
-package fr.uge.structsure.dto.userAccount;
-
-import java.util.Objects;
-
-public record PasswordRequest(String password) {
-    public PasswordRequest {
-        Objects.requireNonNull(password);
-    }
-}
