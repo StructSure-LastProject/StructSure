06b24966aa816fc19cf4cc2f38b643f0107d60de
AchrafQab <achrafqabil2002@gmail.com>
2025-02-22 19:54:18 +0100

    Add sensor on db from popup

---
+++ Android/app/src/main/java/fr/uge/structsure/scanPage/data/AddSensorFormState.kt
@@ -0,0 +1,15 @@
+package fr.uge.structsure.scanPage.data
+
+/**
+ * Form state for the add sensor form.
+ * @param controlChipError error message for the control chip field
+ * @param measureChipError error message for the measure chip field
+ * @param nameError error message for the name field
+ * @param dateError error message for the date field
+ */
+data class AddSensorFormState(
+    val controlChipError: String? = null,
+    val measureChipError: String? = null,
+    val nameError: String? = null,
+    val dateError: String? = null
+)
