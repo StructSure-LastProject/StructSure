ace62d2acbe82a52bda305ae5d7e7f2002e24324
ANASS OUSAID <anassousaidanass@gmail.com>
2025-02-24 17:42:10 +0100

    In progress

--- Backend/src/main/java/fr/uge/structsure/services/SensorService.java
+++ Backend/src/main/java/fr/uge/structsure/services/SensorService.java
@@ -297,5 +297,4 @@ public class SensorService {
         sensorRepository.save(existingSensor);
         return new SensorPositionResponseDTO(request.controlChip(), request.measureChip());
     }
-
 }
